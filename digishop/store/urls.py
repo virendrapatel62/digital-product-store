5cbb7ab54851cc3ca4d09043308b0c747d76ae9b
virendra <patelvirendra62@gmail.com>
2021-04-11 23:00:14 +0530

    user creation function based view

--- digishop/store/urls.py
+++ digishop/store/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, about, contactus
+from .views import home, about, contactus, signup_view
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('about', about),
     path('contact-us', contactus),
+    path('signup', signup_view),
 ]
