176648d25bc830aa661137ed3b2988ff3e1cb1a9
virendra <patelvirendra62@gmail.com>
2021-04-13 16:34:56 +0530

    home list view

--- digishop/store/urls.py
+++ digishop/store/urls.py
@@ -2,9 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views.auth import SignupView, LoginView
-from .views.home import home, contactus, about
+from .views.home import HomeView, contactus, about
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('about', about),
     path('contact-us', contactus),
     path('signup', SignupView.as_view()),
--- digishop/store/views/home.py
+++ digishop/store/views/home.py
@@ -2,15 +2,26 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import FormView
 from store.models import Product
+from django.views import View
+from django.views.generic import ListView
 # Create your views here.
 
 
+class HomeView(ListView):
+    model = Product
+    template_name = 'store/index.html'
+    context_object_name = 'products'
+
+
+'''
 def home(request):
     products = Product.objects.all()
     context = {
         'products': products
     }
     return render(request, template_name='store/index.html', context=context)
+
+'''
 
 
 def about(request):
