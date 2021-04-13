51acd6527f2c489678d2ed3e93d4e180587de09c
virendra <patelvirendra62@gmail.com>
2021-04-13 23:31:45 +0530

    category filter done

--- digishop/store/templatetags/store_tags.py
+++ digishop/store/templatetags/store_tags.py
@@ -8,3 +8,19 @@ register = template.Library()
 def sale_price(price, discount):
     print(price, discount)
     return floor(price - (price * discount * 0.01))
+
+
+@register.simple_tag
+def get_active_class(active_category, category):
+    print(active_category, category)
+    if active_category is '' and category == '':
+        return ' active'
+
+    if active_category is '':
+        return ''
+
+    if int(active_category) == category:
+        return ' active'
+
+    if int(active_category) != category:
+        return ''
--- digishop/store/views/home.py
+++ digishop/store/views/home.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import FormView
-from store.models import Product
+from store.models import Product, Category
 from django.views import View
 from django.views.generic import ListView
 # Create your views here.
@@ -11,6 +11,23 @@ class HomeView(ListView):
     model = Product
     template_name = 'store/index.html'
     context_object_name = 'products'
+
+    def get_context_data(self):
+        category_pk = self.request.GET.get("category")
+        categories = Category.objects.all()
+        if category_pk:
+            products = Product.objects.filter(category=category_pk)
+
+        else:
+            products = Product.objects.all()
+
+        context = {
+            'categories': categories,
+            'products': products,
+
+        }
+
+        return context
 
 
 '''
