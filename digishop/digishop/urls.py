9b4f0876b7848d0998d0c85097ca448712824fb9
virendra <patelvirendra62@gmail.com>
2021-04-13 14:21:41 +0530

    home page

--- digishop/digishop/urls.py
+++ digishop/digishop/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from .settings import MEDIA_ROOT, MEDIA_URL
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls'))
-]
+]+static(MEDIA_URL, document_root=MEDIA_ROOT)
--- digishop/store/views/home.py
+++ digishop/store/views/home.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import FormView
+from store.models import Product
 # Create your views here.
 
 
 def home(request):
-    return render(request, template_name='store/index.html', context={})
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, template_name='store/index.html', context=context)
 
 
 def about(request):
