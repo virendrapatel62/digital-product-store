64010d8998959d7ec528c09b3e7ded5866bc3973
virendra <patelvirendra62@gmail.com>
2021-04-14 23:31:27 +0530

    Signed-off-by: virendra <patelvirendra62@gmail.com>

--- digishop/store/views/details.py
+++ digishop/store/views/details.py
@@ -1,8 +1,14 @@
 from django.shortcuts import HttpResponse, render
 from store.models import Product
 from django.views import View
+from django.views.generic import DetailView
 
 
+class ProductDetailView(DetailView):
+    model = Product
+
+
+'''
 class ProductDetailView(View):
     def get(self, request, slug):
         product = Product.objects.get(slug=slug)
@@ -10,7 +16,7 @@ class ProductDetailView(View):
             'product': product
         }
         return render(request, template_name='store/product_detail.html', context=context)
-
+'''
 
 '''
 
